6d2a2added0047378c35b3fb43a254ea368987e5
nbiish <10566304+nbiish@users.noreply.github.com>
2024-04-18 21:00:40 -0400

    got an unending llama3 and wanted to refresh all models to their lates version fast

---
+++ raycast/scripts/ollama-update-models.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ollama list > my-ollama-models.txt
+
+sed -i 's/:.*//' my-ollama-models.txt
+
+while read line; do
+  for i in $line; do
+    ollama pull $i
+  done
+done < my_ollama_models.txt
