610b3e8bd296a9a658db759349af92ca9826dab7
nbiish <10566304+nbiish@users.noreply.github.com>
2024-04-18 20:58:53 -0400

    new ai chat site!

---
+++ raycast/scripts/open-meta.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open-meta
+# @raycast.mode fullOutput
+# Optional parameters:
+# @raycast.icon 🦿
+# @raycast.description quick open meta ai in default browser
+# @raycast.author nbiish
+# @raycast.authorURL https://raycast.com/nbiish
+
+
+open "https://www.meta.ai/"
