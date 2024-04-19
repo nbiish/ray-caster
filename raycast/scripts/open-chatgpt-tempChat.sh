9b82f5340280818eff24d9015b3a0034a226d255
nbiish <10566304+nbiish@users.noreply.github.com>
2024-04-18 20:58:36 -0400

    temp chats

---
+++ raycast/scripts/open-chatgpt-tempChat.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open-chatgpt
+# @raycast.mode fullOutput
+# Optional parameters:
+# @raycast.icon 🦿
+# @raycast.description quick open chatgpt temporary chat in default browser
+# @raycast.author nbiish
+# @raycast.authorURL https://raycast.com/nbiish
+
+
+open "https://chat.openai.com/"
