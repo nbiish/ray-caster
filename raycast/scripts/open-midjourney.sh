3247857988fdd78578b08769ea789264b62c2b0b
nbiish <10566304+nbiish@users.noreply.github.com>
2024-11-03 21:54:56 -0500

    add script to quickly open Midjourney Explore in the default browser

---
+++ raycast/scripts/open-midjourney.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open-midjourney
+# @raycast.mode fullOutput
+# Optional parameters:
+# @raycast.icon 🎨
+# @raycast.description quick open Midjourney Explore in default browser
+# @raycast.author nbiish
+# @raycast.authorURL https://raycast.com/nbiish
+
+open "https://www.midjourney.com/explore"
