755fba117a078445b1a6e9b6b5f94c3cac61cf08
nbiish <10566304+nbiish@users.noreply.github.com>
2024-12-02 21:32:52 -0500

    added deepseek

---
+++ raycast/scripts/open-deepseek.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open-deepseek
+# @raycast.mode fullOutput
+# Optional parameters:
+# @raycast.icon 🧠
+# @raycast.description quick open deepseek chat in default browser
+# @raycast.author nbiish
+# @raycast.authorURL https://raycast.com/nbiish
+
+open "https://chat.deepseek.com/"
