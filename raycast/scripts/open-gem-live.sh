d3462165c68d597317ad125e9da8a8e70538ad76
nbiish <10566304+nbiish@users.noreply.github.com>
2024-12-13 15:46:25 -0500

    two prompts

---
+++ raycast/scripts/open-gem-live.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title open-gem-live
+# @raycast.mode fullOutput
+# Optional parameters:
+# @raycast.icon 💎
+# @raycast.description quick open gemini live in default browser
+# @raycast.author nbiish
+# @raycast.authorURL https://raycast.com/nbiish
+
+open "https://aistudio.google.com/live"
